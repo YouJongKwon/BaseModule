3bb21ef723b1b618ca0b07baf42dfe15d1a2b189
Jongkwon You <jkyou@hmit.kr>
2023-03-18 14:36:15 +0900

    Github webhooks - push event 확인1

--- app/src/main/java/com/base/basemodule/sample/MainActivity.kt
+++ app/src/main/java/com/base/basemodule/sample/MainActivity.kt
@@ -6,7 +6,7 @@ import com.base.basemodule.databinding.ActivityMainBinding
 import com.base.basemodule.recyclerview.ActivityUser
 import com.base.view.activity.BaseActivity
 
-// main 화면 test3
+// main 화면 test1
 class MainActivity : BaseActivity() {
 
 
